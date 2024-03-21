0a5d2febc7a6c0adee3491fb7cb23f7370d0368d
徐小词 <3268586383@qq.com>
2024-03-21 17:53:10 +0800

    web

--- lib/main.dart
+++ lib/main.dart
@@ -8,10 +8,12 @@ void main() {
     // fileHost: "file.k1.game/",
     fileScheme: "http",
     // fileHost: " http://192.168.6.117:12345/",//本地
-    fileHost: "47.97.250.198:12345/", //阿里
+    // fileHost: "47.97.250.198:12345/", //阿里
+    fileHost: "127.0.0.1:12345/", //web
     apiScheme: "http",
     // apiHost: 'http://192.168.6.117:12345/',//本地
-    apiHost: 'http://47.97.250.198:12345/',//阿里
+    // apiHost: 'http://47.97.250.198:12345/',//阿里
+    apiHost: 'http://127.0.0.1:12345/',//web
     socketHost: "",
     mode: RunMode.profile,
   ));
